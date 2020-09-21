015b77db043aab0447c6f7463435537b55e7134b
AlexandraMihalevschi <68810700+AlexandraMihalevschi@users.noreply.github.com>
2020-09-21 20:04:17 +0300

    Add files via upload

---
+++ Problema_3.py
@@ -0,0 +1,7 @@
+a = 5
+#Perimetrul unei singure suprafete
+print(a*4) 
+#Aria tuturor suprafetelor
+print(6*(a**2))
+#Volumul
+print(a**3)
---
+++ Problema_4.py
@@ -0,0 +1,9 @@
+n = 12
+print("A:")
+print(n*100)
+print("B:")
+print(n*1000)
+print("C:")
+print(n*12)
+print(n*52)
+print(n*356)
