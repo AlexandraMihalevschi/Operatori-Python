0f1e9a3242883e781f67d1ce43d150e00367a497
AlexandraMihalevschi <68810700+AlexandraMihalevschi@users.noreply.github.com>
2020-09-21 20:44:30 +0300

    Add files via upload

---
+++ Problema_5.py
@@ -0,0 +1,7 @@
+
+print(" A     B     A and B    A or B    not A    not B")
+print("------------------------------------------------")
+print(" F     F        F          F        T         T")
+print(" F     T        F          T        T         F")
+print(" T     F        F          T        F         T")
+print(" T     T        T          T        F         F")
